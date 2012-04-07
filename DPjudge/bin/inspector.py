9d11cb628ff09869d7aba9e7f633564fb7f93638
ukdp <ukdp@uk.diplom.org>
2012-04-07 04:11:08 +0100

    Adding browse methods to inspect object, to simplify checking url page errors.

--- DPjudge/bin/inspector.py
+++ DPjudge/bin/inspector.py
@@ -1,4 +1,4 @@
-import re, sys
+import re, sys, urllib
 from DPjudge import *
 from DPjudge.variants.dppd import *
 
@@ -7,28 +7,69 @@ class Inspector:
 	Supporter class for inspect
 	"""
 	def __init__(self):
+		self.game = None
 		self.varNames = dict()
 	def load(self, gameName):
 		import re
 		self.vars = dict()
-		game = Status().load(gameName)
-		if game and game.error: print '\n'.join(game.error)
-		self.vars[self.makeVar(gameName, 'G')] = self.vars['self'] = game
-		for power in game.powers or []:
+		self.game = Status().load(gameName)
+		if self.game and self.game.error: print '\n'.join(self.game.error)
+		self.vars[self.makeVar(gameName, 'G')] = self.vars['self'] = self.game
+		for power in self.game.powers or []:
 			self.vars[self.makeVar(power.name, 'P')] = power
-		print 'Loaded', ('game %s' % gameName, 'no game')[not game]
+		print 'Loaded', ('game %s' % gameName, 'no game')[not self.game]
 		self.makeGlob(self.vars, 2)
-	def loadDb(self):
+	def connect(self):
 		dppd = DPPD()
 		db = dppd.db
 		self.makeGlob(locals(), 2)
+	def visit(self, game = None, power = 'MASTER', password = None):
+		if not game:
+			if not self.game:
+				print 'No game loaded'
+				return
+			game = self.game
+		elif not isinstance(game, Game):
+			game = Status().load(game)
+			if not game:
+				print 'No such game'
+				return
+		query = {'game': game.name}
+		if power: 
+			powerName = power
+			if isinstance(power, Power): powerName = power.name
+			elif power.upper() != 'MASTER' and not [1 for x in game.powers
+				if x.name in (power.upper(), '_' + power.upper())]:
+				print 'No such power'
+				return
+			if not password: password = game.password
+			query.update({'power': powerName, 'password': password})
+		self.browse(query, host.dpjudgeURL, 2)
+	def query(self, query = None):
+		self.browse(query, host.dppdURL, 2)
+	def browse(self, query = None, address = None, depth = 1):
+		if not address: address = host.dpjudgeURL
+		if isinstance(query, dict): query = urllib.urlencode(query)
+		if query: address += ('?', '&')['?' in address] + query
+		page = urllib.urlopen(address)
+		result = page.read()
+		page.close()
+		if 'DPjudge Error' in result:
+			try:
+				trace = result[result.rindex('<!--') + 4:result.rindex('-->')
+					].strip('\n\t')
+				print trace
+			except: print result
+		else: print result
+		self.makeGlob(locals(), depth + 1)
 	def eval(self, expr, depth = 1):
 		for name, var in self.varNames.items():
 			expr = re.sub(r'\b%s\b' % re.escape(name), var, expr)
 		return eval(expr, sys._getframe(depth).f_globals)
 	def makeVar(self, name, initial):
-		var = initial * name[0].isdigit() + re.sub(
-			r'\W', '_', name.lower())
+		var = name.lower()[name[0] == '_':]
+		var = initial * var[0].isdigit() + re.sub(
+			r'\W', '_', var.lower())
 		if var != name: self.varNames[name] = var
 		return var
 	def makeGlob(self, vars, depth = 1):
