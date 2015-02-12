0b767d2d40b6bb99955964c4b5570cada58cfe27
woelpad <woelpad@gmail.com>
2015-02-12 22:53:54 +0900

    Handle strings and unicode separately in Time instance creation.

--- DPjudge/Time.py
+++ DPjudge/Time.py
@@ -2,15 +2,20 @@ class Time(str):
 	#	------------------------------------------------------------------
 	def __new__(self, when = None, npar = 5):
 		try:
-			if (unicode(when, 'latin-1').isnumeric() and
+			if (when.isnumeric() and
 				len(when) in range(8, 16, 2)): return str.__new__(self, when)
 			else: raise AssertionError
 		except:
-			try: return str.__new__(self, '%02d' * npar % when)
+			try:
+				if (unicode(when, 'latin-1').isnumeric() and
+					len(when) in range(8, 16, 2)): return str.__new__(self, when)
+				else: raise AssertionError
 			except:
-				try: return str.__new__(self, '%02d' * npar %
-					time.localtime(when)[:npar])
-				except: return str.__new__(self)
+				try: return str.__new__(self, '%02d' * npar % when)
+				except:
+					try: return str.__new__(self, '%02d' * npar %
+						time.localtime(when)[:npar])
+					except: return str.__new__(self)
 	#	------------------------------------------------------------------
 	def tuple(self):
 		return tuple(map(int, [self[:4]] + [self[x:x+2] or '0'
